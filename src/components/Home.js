58ac4c7c9d66fb6b5071d2ba69be9c70b39ce9f9
Wilker Blanco <wilkerblanco18@gmail.com>
2022-08-14 22:20:41 -0400

    obtenida la cantidad de C en los nombres de los characters

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,27 +2,44 @@ import { useState } from 'react'
 
 function Home() {
   const [characters, setCharacters] = useState([]);
-  function CharCounter() {
+
+  async function CharCounter() {
     /* función para obtener todos los personajes*/
-    let charactersArray=[]
+    let charactersArray = []
     for (var i = 1; i < 43; i++) {
-      fetch(`https://rickandmortyapi.com/api/character/?page=${i}`)
+      await fetch(`https://rickandmortyapi.com/api/character/?page=${i}`)
         .then((response) => response.json())
         .then((data) => {
           /*Se está llenando el arreglo charactersArray con la Data de cada personaje*/
-          data.results.map((character)=>{return charactersArray.push(character)});          
+          data.results.map((character) => {return charactersArray.push(character)});
         });
-    }
-    console.log(charactersArray)
+    };
+    let indices = [];
+    /*Se recorre el arreglo de los characters para obtener cada nombre
+    luego recorrer el nombre para almacenar la posición donde se ecuentra la letra
+    para saber el número de veces que aparece se lee la longitud del arreglo*/
+    for (var i=0; i<charactersArray.length; i++) {
+       console.log(charactersArray[i].name)
+       let nameCharacter=charactersArray[i].name
+      for (var index = 0; index < nameCharacter.length; index++) {
+        if (nameCharacter[index] === "c") indices.push(index);
+      }
+    };
+      var quantityLetterCharacters=indices.length
+      console.log(quantityLetterCharacters)
+
+    setCharacters(charactersArray);
     return
   };
 
-  return (
-    <>
-      <h1>Rick and Morty Challenge</h1>
-      <button type="button" onClick={CharCounter}>Iniciar</button>
-    </>
+return (
+  <>
+    <h1>Rick and Morty Challenge</h1>
+    <button type="button" onClick={() => {
+      CharCounter();
+    }}>Iniciar</button>
+  </>
 
-  );
+);
 }
 export default Home
